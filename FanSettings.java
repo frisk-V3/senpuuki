a8dc6fa9cb7dabfc3ec61c62a1ce48118e1a3947
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-04-17 20:14:57 +0900

    Create FanSettings.java

---
+++ FanSettings.java
@@ -0,0 +1,6 @@
+public class FanSettings {
+    public static final int OFF = 0;
+    public static final int LOW = 1;
+    public static final int MEDIUM = 2;
+    public static final int HIGH = 3;
+}
